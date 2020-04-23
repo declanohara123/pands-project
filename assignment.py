ae219ba9c6e83b5102084201dbc18e304c2191f2
Declan <deciohara@gmail.com>
2020-04-23 11:04:51 +0100

    info

--- assignment.py
+++ assignment.py
@@ -1,5 +1,20 @@
 ## assignment
+#
 # to investigate the Fisher's iris data set.
 # investigate the data
 # come to conclusions based on data
 # print images of data
+#
+# data set of 150 pieces of info
+# 
+# sepal lenght
+# sepal width
+# petal lenght
+# petal width
+# all measureed in cm
+
+# three different species
+# iris setosa
+# iris versicolour
+# iris virginica
+
